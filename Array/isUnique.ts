0896c1c980d9f576c050fb95dbccf432b90b301d
Brian Liang <brian.j.liang@gmail.com>
2019-11-13 20:47:26 -0700

    isUnique question and hints

---
+++ Array/isUnique.ts
@@ -0,0 +1,13 @@
+/**
+Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+
+Hint 1: Try a hast table.
+
+Hint 2: Could a vector be useful?
+
+Hint 3: Can you solve it in O(N log N) time? What might the solution like that look like?
+**/
+
+let isUnique = function(str){
+
+};
