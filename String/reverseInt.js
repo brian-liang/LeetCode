08107533b246c1d74d2ef76cf0db8e15b97e26fa
brian-liang <brian.j.liang@gmail.com>
2019-11-19 15:24:13 -0700

    Reverse Integer solution working

--- String/reverseInt.js
+++ String/reverseInt.js
@@ -22,7 +22,30 @@ Assume we are dealing with an environment which could only store integers within
  * @return {number}
  */
 var reverse = function(x) {
+    let isNegative = x<0;
+    if(isNegative) {
+        if(x < -8463847412){
+            return 0;
+        } else {
+        x = -x;
+        }
+    } else {
+        if(x > 7463847412){
+            return 0;
+        }
+    }
 
+    let revNumber = x % 10;
+    x/=10;
+
+    while(Math.floor(x/10) >0){
+        revNumber = revNumber*10 + Math.floor(x%10);
+        x /= 10;
+    }
+
+    revNumber = revNumber*10 + Math.floor(x%10);
+
+    return isNegative ? -revNumber : revNumber; 
 };
 
 
@@ -31,3 +54,11 @@ var reverse = function(x) {
 console.log(reverse(123));      //returns 321
 console.log(reverse(-123));      //returns -321
 console.log(reverse(120));      //returns 21
+
+//overflow case -2147483648 overflow for negative
+console.log(reverse(-8463847412));      //returns -2147483648
+console.log(reverse(-9463847412));      //returns 0
+
+//overflow case 2147483647 overflow for positive
+console.log(reverse(7463847412));       //returns 2147483647
+console.log(reverse(8463847412));       //returns 0
