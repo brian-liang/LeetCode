f9e77786f6fb6be8eb4c763fe9149550570167c1
brian-liang <brian.j.liang@gmail.com>
2019-11-26 14:56:39 -0700

    hasPalindromePermutation solution

--- Hash/hasPalindromePermutation.js
+++ Hash/hasPalindromePermutation.js
@@ -16,8 +16,21 @@ Examples:
 
 function hasPalindromePermutation(theString){
 
-    return false;
+    var charSet = new Set();
+
+    for (const char of theString){
+        if (charSet.has(char)){
+            charSet.delete(char);
+        } 
+        else{
+            charSet.add(char);
+        }
+    }
+
+    return charSet.size <= 1;
 }
+
+//
 
 
 /*********TEST*******/
@@ -25,3 +38,11 @@ console.log(hasPalindromePermutation("civic"));     //returns true
 console.log(hasPalindromePermutation("ivicc"));     //returns true
 console.log(hasPalindromePermutation("civil"));     //return false
 console.log(hasPalindromePermutation("livci"));     //returns false
+
+
+/*********Complexity*****/
+/*
+O(n) time, since we're making one iteration through the nnn characters in the string.
+
+Our unpairedCharacters set is the only thing taking up non-constant space. We could say our space cost is O(n)O(n)O(n) as well, since the set of unique characters is less than or equal to nnn. But we can also look at it this way: there are only so many different characters. How many? The ASCII character set has just 128 different characters (standard english letters and punctuation), while Unicode has 110,000 (supporting several languages and some icons/symbols). We might want to treat our number of possible characters in our character set as another variable kkk and say our space complexity is O(k)O(k)O(k). Or we might want to just treat it as a constant, and say our space complexity is O(1)O(1)O(1). 
+*/
