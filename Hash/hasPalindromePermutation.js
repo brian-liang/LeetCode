cd93c50d376509e804542ee205da14255a51b177
brian-liang <brian.j.liang@gmail.com>
2019-11-26 13:32:01 -0700

    hasPalindromePermutation question setup

---
+++ Hash/hasPalindromePermutation.js
@@ -0,0 +1,27 @@
+/*
+InterviewCake
+
+ Write an efficient function that checks whether any permutation ↴ of an input string is a palindrome. ↴
+
+You can assume the input string only contains lowercase letters.
+
+Examples:
+
+    "civic" should return true
+    "ivicc" should return true
+    "civil" should return false
+    "livci" should return false
+
+*/
+
+function hasPalindromePermutation(theString){
+
+    return false;
+}
+
+
+/*********TEST*******/
+console.log(hasPalindromePermutation("civic"));     //returns true
+console.log(hasPalindromePermutation("ivicc"));     //returns true
+console.log(hasPalindromePermutation("civil"));     //return false
+console.log(hasPalindromePermutation("livci"));     //returns false
